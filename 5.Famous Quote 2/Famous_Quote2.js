0a3092cfc77e2cac904a5ee979dea1d090865c87
Bhhp123 <144653632+Bhhp123@users.noreply.github.com>
2024-03-09 11:55:57 +0500

    Delete 5.Famous Quote 2 directory

--- 5.Famous Quote 2/Famous_Quote2.js
+++
@@ -1,3 +0,0 @@
-var Famous_person = 'Albiert Estien';
-var message = "".concat(Famous_person, " once said, A person who never made a mistake never tried anything new.");
-console.log(message);
--- 5.Famous Quote 2/Famous_Quote2.ts
+++
@@ -1,3 +0,0 @@
-let Famous_person:string ='Albiert Estien';
-let message:string = `${Famous_person} once said, A person who never made a mistake never tried anything new.`;
-console.log(message);
