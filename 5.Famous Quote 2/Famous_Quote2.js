fd9344ad047bcdbb8442a6ab13e79acc46fa76ed
Bhhp123 <144653632+Bhhp123@users.noreply.github.com>
2024-03-09 11:57:15 +0500

    Add files via upload

---
+++ 5.Famous Quote 2/Famous_Quote2.js
@@ -0,0 +1,4 @@
+var Famous_person = 'Albiert Estien';
+var quote = '" A person who never made a mistake never tried anything new."';
+var message = "".concat(Famous_person, " once said, ").concat(quote);
+console.log(message);
---
+++ 5.Famous Quote 2/Famous_Quote2.ts
@@ -0,0 +1,4 @@
+let Famous_person:string ='Albiert Estien';
+let quote:string='" A person who never made a mistake never tried anything new."'
+let message:string = `${Famous_person} once said, ${quote}`;
+console.log(message);
