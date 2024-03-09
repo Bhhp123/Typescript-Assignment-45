225a98525df9a02e1a0452b22b71fa4120397c03
Bhhp123 <144653632+Bhhp123@users.noreply.github.com>
2024-03-09 11:55:43 +0500

    Delete 4.Famous Quote directory

--- 4.Famous Quote/Famous_Quote.js
+++
@@ -1,2 +0,0 @@
-var Myquote = 'Albert Esintein once said, "A person who never made a mistake never tried anything new."';
-console.log(Myquote);
--- 4.Famous Quote/Famous_Quote.ts
+++
@@ -1,2 +0,0 @@
-let Myquote:string='Albert Esintein once said, "A person who never made a mistake never tried anything new."'; 
-console.log(Myquote);
