c8acea6860e2c8c8bdf7e9e2324b65c8950324ee
Bhhp123 <144653632+Bhhp123@users.noreply.github.com>
2024-03-09 11:01:19 +0500

    Delete 3.Name Case directory

--- 3.Name Case/Name_Case.js
+++
@@ -1,7 +0,0 @@
-var person_name = 'Bilawal';
-console.log(person_name.toUpperCase());
-console.log(person_name.toLowerCase());
-var UpperCase = person_name.charAt(0).toUpperCase();
-var LowerCase = person_name.slice(1).toLowerCase();
-var TitleCase = UpperCase + LowerCase;
-console.log(TitleCase);
