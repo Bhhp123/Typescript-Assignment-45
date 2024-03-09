3d7a188e35fcc7aeee2b3d9fee8b2e79a2642a3e
Bhhp123 <144653632+Bhhp123@users.noreply.github.com>
2024-03-09 11:19:39 +0500

    Add files via upload

---
+++ 10.Adding Comments/adding_comments.js
@@ -0,0 +1,7 @@
+//Name: 'Bilawal Hussain'
+//Date: '17/02/2014'
+//Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results?
+console.log(6 + 2); //addition
+console.log(10 - 2); //subtraction
+console.log(4 * 2); //multiplication
+console.log(16 / 2); //division 
